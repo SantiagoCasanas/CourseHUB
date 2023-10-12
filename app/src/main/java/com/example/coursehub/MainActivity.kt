a03382e911131ad63b0d4c70cb430c6b0e04523a
SantiagoCasanas <santiago01.02.003@gmail.com>
2023-10-12 16:27:45 -0500

    Boton y colores

--- app/src/main/java/com/example/coursehub/MainActivity.kt
+++ app/src/main/java/com/example/coursehub/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.coursehub.ui.theme.CourseHUBTheme
 import com.example.coursehub.users.signUpScreen
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = colorResource(id = R.color.Backgorund_down)
                 ) {
                     signUpScreen()
                 }
@@ -31,20 +32,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CoursePreview() {
     CourseHUBTheme {
-
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = colorResource(id = R.color.Background_up)
+        ) {
+            signUpScreen()
+        }
     }
 }
--- app/src/main/java/com/example/coursehub/users/LoginAndSignUpScreen.kt
+++ app/src/main/java/com/example/coursehub/users/LoginAndSignUpScreen.kt
@@ -5,15 +5,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -23,22 +20,25 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.coursehub.R
 
 @Composable
 fun signUpScreen(
@@ -52,29 +52,30 @@ fun signUpScreen(
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        // Contenedor azul para el texto "Login"
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color(31,31,57)) // Color azul
+                .background(colorResource(id = R.color.Backgorund_down)) // Color azul
                 .padding(40.dp)
         ) {
             Text(
                 text = text,
-                fontSize = 24.sp,
-                color = Color.White
+                fontSize = 40.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
             )
         }
-        // Contenedor para los demás componentes
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 100.dp).background(color = Color(47,47,66))
+                .padding(top = 130.dp).background(color = colorResource(id = R.color.Background_up))
+                .clip(shape = RoundedCornerShape(15.dp))
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
+                Spacer(modifier = Modifier.height(15.dp))
                 if (showLoginForm.value) {
                     UserLoginForm(isCreatedAccount = false) { email, password ->
                         viewModel.logIn(email, password)
@@ -93,13 +94,16 @@ fun signUpScreen(
                     val text1 = if (showLoginForm.value) "I don't have an account" else "I already have an account"
                     val text2 = if (showLoginForm.value) "Sign Up" else "Login"
 
-                    Text(text = text1)
+                    Text(
+                        text = text1,
+                        color = Color.White,
+                    )
                     Text(
                         text = text2,
                         modifier = Modifier
                             .clickable { showLoginForm.value = !showLoginForm.value }
                             .padding(start = 5.dp),
-                        color = MaterialTheme.colorScheme.secondary
+                        color = colorResource(id = R.color.Buttom)
                     )
                 }
             }
@@ -118,7 +122,7 @@ fun UserLoginForm(isCreatedAccount: Boolean = false, onDone: (String, String) ->
     ) {
         // Etiqueta para Email
         Text(
-            text = "Email",
+            text = "Your Email",
             color = Color.White, // Color del texto en blanco
             modifier = Modifier.padding(top = 8.dp)
         )
@@ -217,17 +221,20 @@ fun SubmitButton(textId: String, onClick: ()->Unit) {
     Button(
         onClick = onClick,
         modifier = Modifier
-            .padding(3.dp)
-            .fillMaxWidth(0.5f)
-            .height(48.dp), // Ajusta la altura del botón según tus preferencias
-        shape = RoundedCornerShape(8.dp), // Bordes redondeados
+            .padding(10.dp)
+            .fillMaxWidth(1f)
+            .height(48.dp),
+        shape = RoundedCornerShape(8.dp),
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color(61,92,255) // Color del texto en blanco
+            containerColor = colorResource(id = R.color.Buttom)
         )
     ) {
-        Text(text = textId,
-        modifier = Modifier
-            .padding(5.dp))
+        Text(
+            text = textId,
+            fontSize = 25.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
@@ -279,9 +286,15 @@ fun EmailInput(emailState: MutableState<String>, labelId: String = "Email") {
 }
 
 @Composable
-fun InputField(valueState: MutableState<String>, labelId: String, keyboardType: KeyboardType, isSingleLine: Boolean = true) {
-    androidx.compose.material.OutlinedTextField(value = valueState.value,
-        onValueChange ={valueState.value=it},
+fun InputField(
+    valueState: MutableState<String>,
+    labelId: String,
+    keyboardType: KeyboardType,
+    isSingleLine: Boolean = true
+) {
+    TextField(
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
         label = { Text(text = labelId) },
         singleLine = isSingleLine,
         modifier = Modifier
@@ -289,6 +302,7 @@ fun InputField(valueState: MutableState<String>, labelId: String, keyboardType: 
             .fillMaxWidth(),
         keyboardOptions = KeyboardOptions(
             keyboardType = keyboardType
-        )
+        ),
+        textStyle = TextStyle(color = Color.White)
     )
 }
